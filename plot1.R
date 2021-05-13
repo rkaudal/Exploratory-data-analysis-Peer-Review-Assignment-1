232de723cedaf9d5cb84ca5355ae8c11fcfbf7b0
Beginner <80064271+rkaudal@users.noreply.github.com>
2021-05-12 21:31:52 -0500

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,11 @@
+#code for plot1
+fileName <- file("household_power_consumption.txt") 
+
+# reading the data for 2007-02-01 and 2007-02-02
+data <- read.table(text = grep("^[1,2]/2/2007", readLines(fileName), value = TRUE), col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), sep = ";", header = TRUE)
+
+# opening PNG device
+png(file = "plot1.png", width = 480, height = 480, units = "px")
+
+# generating Plot 1
+hist(data$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab="Frequency")
